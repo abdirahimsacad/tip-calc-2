d9788710c60f0689b3ac46dcbcd5a9b493b5d2ce
abdirahimsacad <abdirahiimsacad@gmail.com>
2023-07-28 08:45:18 -0700

    update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("hello");
+function calcTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+      return bill * 0.15;
+    } else {
+      return bill * 0.20;
+    }
+  }
+  const bill = 200;
+  const tip = calcTip(bill);
+
+  console.log(`The bill is ${bill}, and the tip amount is ${tip}.`);
